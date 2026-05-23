4025b6ef726efc6a126b978dab74ad8c9f42944b
Dacosta14 <jc854406@gmail.com>
2026-05-23 20:39:20 -0300

    Att3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
+
 import IntroScene from "./scenes/IntroScene"
 import TunnelScene from "./scenes/TunnelScene"
 import OtherWorldScene from "./scenes/OtherWorldScene"
 
 function App() {
+
   const [scene, setScene] = useState("intro")
 
+  const audioRef = useRef(null)
+
+useEffect(() => {
+
+  const audio = new Audio("/music.mp3")
+
+  audio.loop = true
+  audio.volume = 0.22
+
+  audioRef.current = audio
+
+  audio.play().catch(() => {
+    console.log("autoplay bloqueado")
+  })
+
+  return () => {
+    audio.pause()
+  }
+
+}, [])
   return (
     <>
       {scene === "intro" && (
